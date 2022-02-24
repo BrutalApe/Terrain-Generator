f09aaa626106f6b392f2bd57cd6b4a8706e4152e
BrutalApe <75955942+BrutalApe@users.noreply.github.com>
2022-02-24 00:44:03 -0500

    Add files via upload

--- terrain_gen_2.py
+++ terrain_gen_2.py
@@ -15,6 +15,7 @@ from random import randint
 from time import sleep
 
 def create_cube(name, x_loc, y_loc, z_loc, x_scl, y_scl, z_scl):
+    print("Creating cube...")
     O.mesh.primitive_cube_add(location=(x_loc,y_loc,z_loc))
     O.transform.resize(value=(x_scl, y_scl, z_scl))
     
@@ -22,7 +23,17 @@ def create_cube(name, x_loc, y_loc, z_loc, x_scl, y_scl, z_scl):
         if (obj.type == "MESH") and (obj.name == "Cube"):
             obj.name = name
 
+def create_plane(name, x_loc, y_loc, z_loc, x_scl, y_scl):
+    print("Creating plane...")
+    O.mesh.primitive_plane_add(location=(x_loc,y_loc,z_loc))
+    O.transform.resize(value=(x_scl, y_scl, 0))
+    
+    for obj in C.selected_objects:
+        if (obj.type == "MESH") and (obj.name == "Plane"):
+            obj.name = name
+
 def subdivide_face(object_name, face, num_of_cuts):
+    print("Subdividing face...")
     for obj in C.scene.objects:
         if obj.name == object_name:
             obj.select_set(True)
@@ -40,10 +51,33 @@ def subdivide_face(object_name, face, num_of_cuts):
     O.object.mode_set(mode="EDIT")
     O.mesh.subdivide(number_cuts=num_of_cuts)
     O.object.mode_set(mode="OBJECT")
+    
+def subdivide_plane(object_name, num_of_cuts):
+    print("Subdividing plane...")
+    for obj in C.scene.objects:
+        if obj.name == object_name:
+            obj.select_set(True)
 
-def modify_face_vertices(object_name, size, count, z_min, z_max):
-    vertex_ids = [(size*size)+1] * count  
-    banned_vertex_ids = [0, 2, 4, 6]
+    obj = bpy.context.active_object
+
+    O.object.mode_set(mode="EDIT")
+    O.mesh.subdivide(number_cuts=num_of_cuts)
+    O.object.mode_set(mode="OBJECT")
+
+def adjust_vertex_height(obj, vertex_array, vertex_id, new_height):
+    obj.data.vertices[vertex_id].select = True
+    O.object.mode_set(mode="EDIT")
+    O.transform.translate(value=(0, 0, (new_height)))
+    O.mesh.select_all(action = 'DESELECT')
+    O.object.mode_set(mode="OBJECT")
+    vertex_array[vertex_id] = new_height
+
+def modify_face_vertices(object_name, vertex_array, size, count, z_min, z_max):
+    print("Modifying face vertices...")
+    vertex_ids = [((size+1)*(size+1))+1] * count  
+    
+    #33 14 15 24 18 17
+    banned_vertex_ids = []
     
     for obj in C.scene.objects:
         if obj.name == object_name:
@@ -59,23 +93,24 @@ def modify_face_vertices(object_name, size, count, z_min, z_max):
     i = 0
     while(i < count):
         vertex_id = randint(0, size*size)
+        vertex_id = 110
         print(vertex_id)
-        
+                
         if vertex_id in vertex_ids:
             print("oops, skipping...")
             continue
-        if vertex_id in banned_vertex_ids:
+#        if vertex_id in banned_vertex_ids:
+        if (vertex_id - 10 < 45):
             print("bruh, no way...")
             continue
         
         vertex_ids[i] = vertex_id
         z_scl = randint(z_min, z_max)
-                        
-        obj.data.vertices[vertex_id].select = True
-        O.object.mode_set(mode="EDIT")
-        O.transform.translate(value=(0, 0, (.25*z_scl)))
-        O.mesh.select_all(action = 'DESELECT')
-        O.object.mode_set(mode="OBJECT")
+        adjust_vertex_height(obj, vertex_array, vertex_id, 0.25*z_scl)
+        adjust_vertex_height(obj, vertex_array, vertex_id+1, 0.15*z_scl)
+        adjust_vertex_height(obj, vertex_array, vertex_id-1, 0.15*z_scl)
+        adjust_vertex_height(obj, vertex_array, vertex_id+(size-1), 0.15*z_scl)
+        adjust_vertex_height(obj, vertex_array, vertex_id-(size-1), 0.15*z_scl)
         i += 1
 
 def select_all_meshes():
@@ -100,16 +135,19 @@ def main():
     base_size = 10
     base_x_scl = base_size
     base_y_scl = base_size
-    create_cube("Base", 0, 0, -0.1, base_x_scl, base_y_scl, 0.1)
-    subdivide_face("Base", 5, base_size)
+#    create_cube("Base", 0, 0, -0.1, base_x_scl, base_y_scl, 0.1)
+    create_plane("Base", 0, 0, 0, base_x_scl, base_y_scl)
+    subdivide_plane("Base", base_size-1)
 
-    z_min = 1
+    z_min = 3
     z_max = 5
-    count = 10 # 30 is the highest safe value, i think. at least for 10x10.
+    count = 1 # 30 is the highest safe value, i think. at least for 10x10.
     # should find a way to calculate it algorithmically
     
-    modify_face_vertices("Base", base_size, count, z_min, z_max)
-
+    vertex_array = [0] * (base_size + 1) * (base_size + 1)
+    
+    modify_face_vertices("Base", vertex_array, base_size, count, z_min, z_max)
+    print(vertex_array)
 
 if __name__ == "__main__":
     main()
